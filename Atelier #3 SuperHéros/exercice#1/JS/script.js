0b3c4d82a7c0f9a8d94eb588969313dd4aa810ae
Matis Larouche <LarMa1933473@etu.cegepjonquiere.ca>
2019-10-31 10:18:52 -0400

    image exercise 1

---
+++ Atelier #3 SuperHéros/exercice#1/JS/script.js
@@ -0,0 +1,16 @@
+function afficherHeros1_onclick()
+{
+    document.querySelector("img").src="images/super-hero1.jpg";
+}
+function afficherHeros2_onclick()
+{
+    document.querySelector("img").src="images/super-hero2.jpg";
+}
+function afficherHeros3_onclick()
+{
+    document.querySelector("img").src="images/super-hero3.jpg";
+}
+function afficherHeros4_onclick()
+{
+    document.querySelector("img").src="images/super-hero4.jpg";
+}
