c34d0e00bfbc5fa6e302b7972bd38992c9dac5f5
Matis Larouche <LarMa1933473@etu.cegepjonquiere.ca>
2019-11-10 16:39:29 -0500

    desactiver les image

--- Atelier #4 SuperHéros/JS/script.js
+++ Atelier #4 SuperHéros/JS/script.js
@@ -3,32 +3,36 @@ function radHero_onclick(noImage)
 
     document.querySelector("#imgHero").src="images/super-hero"+ noImage +".jpg";
 
+    document.querySelector( "#imgHero"+noImage ).disabled = true;
+
 }
 function desactiverBtn_onclick ()
 {
-    for(var i=10;i<=4;i++)
+
+    for(var i=1;i<=4;i++)
     {
-        document.querySelector( "#imgHero" +i ).disabled = true;
+        document.querySelector( "#imgHero"+i ).disabled = true;
     }
 }
 function activerBtn_onclick  ()
 {
-    for(var i=0;i<=4;i++)
+    for(var i=1;i<=4;i++)
     {
         document.querySelector( "#imgHero" +i).disabled = false;
     }
 }
 function memeImage_onclick  ()
 {
-    for(var i=0;i<=4;i++)
+    for(var i=1;i<=4;i++)
     {
         document.querySelector("#imgHero" +i).src="images/super-hero1.jpg";
     }
 }
 function desactiverClick_onclick  ()
 {
-    for(var i=0;i<=4;i++)
+    for(var i=1;i<=4;i++)
     {
         document.querySelector( "#imgHero" +i).disabled = true;
     }
+
 }
