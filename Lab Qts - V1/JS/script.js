a61d487e53e304c5916c9b32e32fe9fe26f017ed
Matis Larouche <LarMa1933473@etu.cegepjonquiere.ca>
2019-11-14 10:18:05 -0500

    reglage des boucle for qui ne fonctionnais pas

---
+++ Lab Qts - V1/JS/script.js
@@ -0,0 +1,27 @@
+
+var tabReponseUsager = new Array('A','A','A','A','A');
+function noQts_onchange(noQTS)
+{
+    tabReponseUsager[noQTS-1]=document.queryselector("#lstQts"+noQts).value;
+}
+function btnCorriger_onclick()
+{
+    var tabBon = new Array("A","C","B","A","B");
+    var point;
+    var sexMale=M.;
+    var sexFem=Mme.;
+
+    for(var i=0;i<=5;i++)
+    {
+        if(tabReponseUsager[i] === tabBon[i])
+        {
+            point+=20;
+        }
+    }
+    if(documment.querySelector(#radMasc).checked==true)
+    {
+
+    }
+}
+
+
