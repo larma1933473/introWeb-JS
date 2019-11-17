b195765f8dc6c2e2c069f3c97b6b63b65dd18e0d
Matis Larouche <LarMa1933473@etu.cegepjonquiere.ca>
2019-11-17 18:02:54 -0500

    resultat est toujours 0

---
+++ Lab Qts -V2/JS/script.js
@@ -0,0 +1,48 @@
+var tabReponseUsager = new Array('A','A','A','A','A');
+var point=0;
+
+function btnCorriger_onclick()
+{
+    calculePointage();
+    reponsePointage();
+    point=0;
+
+}
+function calculePointage ()
+{
+    tabReponseUsager = document.querySelectorAll(".lstQts");
+
+    var tabBon = new Array("A","C","B","A","B");
+
+
+    for(var i=0;i<tabBon.length;i++)
+    {
+        if(tabReponseUsager[i] == tabBon[i])
+        {
+            point=point+20;
+        }
+    }
+
+}
+function reponsePointage()
+{
+    var nom= document.querySelector("#txtNom").value;
+
+    if(document.querySelector("#radMasc").checked == true && point > 79 )
+    {
+        document.querySelector("#lblReponse").innerHTML =("brabo M. "+nom+ " vous avez obtenue "+ point+"/100");
+    }
+    else if(document.querySelector("#radMasc").checked ==true )
+    {
+        document.querySelector("#lblReponse").innerHTML = ("desoler M. "+nom+ " vous avez obtenue "+point+"/100");
+    }
+    else if(document.querySelector("#radFem").checked ==true && point > 79 )
+    {
+        document.querySelector("#lblReponse").innerHTML = ("bravo Mme. "+nom+ " vous avez obtenue "+point+"/100");
+    }
+    else
+    {
+        document.querySelector("#lblReponse").innerHTML = ("desoler Mme. "+nom+ " vous avez obtenue "+point+"/100");
+    }
+
+}
