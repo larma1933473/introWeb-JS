88f26e1be7cf124418626c37b59e4bc39f60ffcd
Matis Larouche <LarMa1933473@etu.cegepjonquiere.ca>
2019-11-25 09:50:16 -0500

    exercise fonctionne

---
+++ Exercices expressions régulières/JS/script.js
@@ -0,0 +1,47 @@
+// https://regexr.com/
+//----------------------------------------------------------------------------------//
+function btnValider_onclick()
+{   var message;
+	if (validerCar(document.querySelector("#txtValidation").value ) === true)
+	   message ="bon";
+	else
+	   message ="pas bon";
+    document.querySelector("#lblMessage").innerHTML = message;
+}
+
+function valideAnnee(Chaine) // représente une année à quatre chiffres 
+{
+	return /^\d{4}$/.test(Chaine);
+}
+
+function validerNo( Chaine ) // un chiffre peut etre 0 5 25 200 etc..
+{
+    return /^[0]$/.test(Chaine);
+}
+
+function validerNAS( Chaine ) // numéro d'assurance social 418 789 123, tester votre NAS
+{
+    return /^\d{3} \d{3} \d{3}$/.test(Chaine);
+
+}
+
+function validerTel( Chaine )  // au format (418) 876-8373 , tester votre numéro de téléphone
+{
+    return /^\(\d{3}\) \d{3}\-\d{3}/.test(Chaine);
+}
+
+
+function verifierEntreDixTrente( Chaine )//un chiffre entre 10 et 30
+{
+    return /^([1-2][0-9]|30)$/.test(Chaine);
+}
+
+function verifierEntreUnVingt( Chaine )//un chiffre entre 1 et 20
+{
+    return /^([1-9]|1[[0-9]|20)$/.test(Chaine);
+}
+
+function validerCar( Chaine ) // des chaînes de caractères alphabétiques majuscule et minuscule et accepte le trait d'union pas obligatoire
+{
+    return /^([A-z]|[À-ë])+(\-)?([A-z]|[À-ë])+$/.test(Chaine);
+}
